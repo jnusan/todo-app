ed1c4fdcb1284ab5451a78468492b2347f81dad5
jnusan <joseenrique0595@gmail.com>
2022-06-03 15:18:08 -0600

    Function to add a task to the todo List Start the todo list with json data

--- App.tsx
+++ App.tsx
@@ -1,22 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import todoList from './data/todoList.json';
 import { Task } from './src/components/task';
+import { ITaskProp } from './src/interfaces/ITaskProp';
 
 export default function App() {
+
+  // Here I can use the useReducer instead of useState to handle the state, when it's an object
+  const [task, setTask] = useState<ITaskProp>({name: '', isDone: false});
+  const [tasks, setTasks] = useState<ITaskProp[]>(todoList);
+
+  const handleAddTask = () => {
+    setTasks([...tasks, task]);
+    setTask({name: '', isDone: false});
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>TODO List</Text>
         <View style={styles.items}>
-
           {
-            todoList.map((x) => {
-              return <Task name={x.name} isDone={x.isDone}></Task>
+            tasks.map((x, i) => {
+              return <Task name={x.name} isDone={x.isDone} key={i}></Task>
             })
           }
         </View>
       </View>
+
+      {/* This input test could be a new component */}
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={"Write a task"} value={task.name} onChangeText={text => setTask({ name: text, isDone: false})}></TextInput>
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText} onPress={() => handleAddTask()}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -36,5 +60,34 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    aligItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+  },
+  addText: {
+
   }
 });
