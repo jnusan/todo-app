054fa95d67585c6bfc1a55c696ee32fe5d822130
jnusan <joseenrique0595@gmail.com>
2022-06-03 14:17:44 -0600

    Task component style

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -20,14 +20,36 @@ export const Task = (props:ITaskProp) => {
 
 const styles = StyleSheet.create({
   item: {
-
+    backgroundColor: '#FFFF',
+    padding: 15,
+    borderRadius:10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20
   },
   itemLeft: {
-
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap'
   },
   square: {
+    width: 24,
+    height: 24,
+    backgroundColor: '#55BCF6',
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15
+  },
+  itemText: {
+    maxWidth:'80%',
 
   },
-  itemText: {},
-  circular: {}
+  circular: {
+    width: 15,
+    height: 15,
+    borderColor: '#55BCF6',
+    borderWidth: 2,
+    borderRadius: 5,
+  }
 });
